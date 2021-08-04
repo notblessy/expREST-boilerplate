ed47d861e19e85a021244160ea6fda71543536d7
Frederich Blessy <frederichblessy@gmail.com>
2021-08-04 23:29:44 +0700

    Add other controllers in auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -84,3 +84,171 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  const rules = {
+    email: 'required',
+    password: 'required',
+  };
+
+  const errors = await validateAll(req.body, rules);
+  if (errors) {
+    return res.json({
+      success: false,
+      message: errors,
+    });
+  }
+
+  try {
+    const existing = await db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!existing) {
+      return res.json({
+        success: false,
+        message: 'Email tidak ditemukan. Daftar dahulu',
+      });
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, existing.password);
+
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: 'Password tidak cocok.',
+      });
+    }
+
+    const payload = {
+      identity: existing.id,
+      user_claims: {
+        id: existing.id,
+        email: existing.email,
+        role: existing.role,
+      },
+    };
+
+    const jwtOptions = {
+      issuer: config.JWT_ISSUER,
+      subject: 'access',
+      algorithm: config.JWT_ALGORITHM,
+    };
+
+    const token = jwt.sign(payload, config.JWT_SECRET, jwtOptions);
+
+    return res.json({
+      type: 'Bearer',
+      token: token,
+      data: existing,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      success: false,
+      message: 'Terjadi Kesalahan.',
+    });
+  }
+};
+
+exports.profile = async (req, res) => {
+  try {
+    const user = await db.User.findOne({
+      where: {
+        id: req.user.identity,
+      },
+    });
+    return res.json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      success: false,
+      message: 'Terjadi Kesalahan.',
+    });
+  }
+};
+
+exports.edit = async (req, res) => {
+  try {
+    const user = await db.User.update(
+      { name: req.body.name, email: req.body.email },
+      {
+        where: {
+          id: req.user.identity,
+        },
+      }
+    );
+    return res.json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      success: false,
+      message: 'Terjadi Kesalahan.',
+    });
+  }
+};
+
+exports.editPassword = async (req, res) => {
+  const rules = {
+    old_password: 'required',
+    new_password: 'required',
+  };
+
+  const errors = await validateAll(req.body, rules);
+  if (errors) {
+    return res.json({
+      success: false,
+      message: errors,
+    });
+  }
+
+  try {
+    const existing = await db.User.findOne({
+      where: {
+        id: req.user.identity,
+      },
+    });
+
+    const isMatch = await bcrypt.compare(
+      req.body.old_password,
+      existing.password
+    );
+
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: 'Password salah.',
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(req.body.new_password, salt);
+
+    const user = await db.User.update(
+      { password: hashed },
+      {
+        where: {
+          id: req.user.identity,
+        },
+      }
+    );
+    return res.json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      success: false,
+      message: 'Something went wrong.',
+    });
+  }
+};
