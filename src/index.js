891fbf6377df3b598e157fdb8a0fe28c57d1a5bf
Frederich Blessy <frederichblessy@gmail.com>
2021-08-08 13:10:10 +0700

    Add unit test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const config = require('./config');
-const routes = require('./routes');
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(routes);
+const app = require('./server');
 
 app.listen(config.PORT, () =>
   console.info(
